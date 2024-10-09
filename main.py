9160154c4696d5be31d6b07dc4bb4f7461cd4b1f
Arthur Wallace Silva Lopes <arthur.lopes@serpro.gov.br>
2024-10-09 11:36:24 -0300

    >

--- main.py
+++ main.py
@@ -487,13 +487,42 @@ def main():
     # Lendo o arquivo CSV diretamente da URL com a codificação 'latin1'
     url_csv = f"https://venda-imoveis.caixa.gov.br/listaweb/Lista_imoveis_{UF}.csv?245598167"
     # Defina os headers para simular um navegador
+    cookies = {
+    '__uzmc': '72194867164301',
+    '__uzmd': '1728479185',
+    'SIMOV': 'ffffffff09ca9ec845525d5f4f58455e445a4a423660',
+    '_ga': 'GA1.1.443502605.1708610501',
+    '_ga_RD3F5P2Z1Q': 'GS1.3.1728407229.69.0.1728407229.60.0.0',
+    '_ga_PD5EBJFQ7X': 'GS1.1.1728478652.158.1.1728479187.0.0.0',
+    'ASPSESSIONIDAAADCART': 'BKEOMLJCHBLFGLKFGOJHNLPG',
+    'ASPSESSIONIDCCTCBBQS': 'DNMPKCADGHOMCHNOALBMIGFD',
+    # Continue adicionando os cookies necessários...
+    }
+
+    # Headers copiados
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'Cache-Control': 'no-cache',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'Pragma': 'no-cache',
+        'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'cross-site',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'X-Frame-Options': 'same-origin',
+        'X-Xss-Protection': '1; mode=block',
+        # Adicione mais headers se necessário...
     }
 
     # Faça a solicitação GET com os headers
     try:
-        response = requests.get(url_csv, headers=headers)
+        response = requests.get(url_csv, headers=headers, cookies=cookies)
         print(response)
         # Verifique se a solicitação foi bem-sucedida
         if response.status_code == 200:
